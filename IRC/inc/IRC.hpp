8c36a8c44548b1c2c1ef01e88a0df8411d75bba7
Miguel Mac <miguelescobar704@gmail.com>
2024-04-15 12:41:40 +0000

    Ajout des s librairies pour les fonctions

---
+++ IRC/inc/IRC.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IRC.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mescobar <mescobar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 09:49:14 by mescobar          #+#    #+#             */
+/*   Updated: 2024/04/15 10:10:32 by mescobar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "iostream"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "netdb.h"
+#include "csignal"
+#include "signal.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "poll.h"
+
+typedef struct	s_data{
+}				t_data;
