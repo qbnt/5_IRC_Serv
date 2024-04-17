45bc6ed13b38668ddecf2d6178b0033afc3f64a7
Alias <quentin.banet@hotmail.fr>
2024-04-17 15:46:27 +0000

    À voir avec @6lence pour les interactions client/serv par le billet d'une classe Channel.

--- IRC/includes/Client.hpp
+++ IRC/includes/Client.hpp
@@ -6,39 +6,61 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:50:22 by mescobar          #+#    #+#             */
-/*   Updated: 2024/04/17 14:41:12 by qbanet           ###   ########.fr       */
+/*   Updated: 2024/04/17 15:43:04 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
 #include "IRC.hpp"
+
+//----------------------------------------------------------------------------||
+
+typedef typename std::vector<Channel *>::iterator ChanVecIt;
+
+//----------------------------------------------------------------------------||
 
 class Client{
 	private:
+		// * Utiles
 		int						_fd;
+		bool					_isConnected;
+
+		// * Infos Serv
+		Server *				_serv;
 		std::vector<Channel *>	_usrChan;
 
+		// * Infos Client
 		std::string				_pseudo;
+		std::string				_password;
 		std::string				_username;
 		std::string				_fullname;
 
-		Client();
-		Client(Client const&);
-		Client& operator=(Client const&);
+		// Client();
+		// Client(Client const&);
+		// Client& operator=(Client const&);
 
 	public:
-		Client(int fd, std::string const & pseudo);
+		Client(int fd, Server *, std::string const & pseudo);
 		~Client();
 
-		int						getClientFd() const	{return _fd; };
-		std::vector<Channel *>	getUsrChan() const	{return _usrChan; };
+		// * Fonctions
+		void	linkSetMsg() const;
+		void	joinChan(Channel *);
+		void	leaveChan(Channel *, bool kicked, std::string const& reason);
 
-		std::string const &		getPseudo() const	{return _pseudo; };
-		std::string const & 	getUsername() const	{return _username; };
-		std::string const &		getFullname() const	{return _fullname; };
+		// * Getters
+		int const						getClientFd() const	{return _fd; };
+		std::vector<Channel *> const	getUsrChan() const	{return _usrChan; };
+		bool const						isConnected() const	{return _isConnected; };
 
-		void					setPseudo(std::string const & ps)		{_pseudo = ps; };
-		void					setUsername(std::string const & us)	{_username = us; };
-		void					setFullname(std::string const & fu)	{_fullname = fu; };
+		std::string const &				getPseudo() const	{return _pseudo; };
+		std::string const &				getPassword() const	{return _password; };
+		std::string const & 			getUsername() const	{return _username; };
+		std::string const &				getFullname() const	{return _fullname; };
+
+		// * Setters
+		void		setPseudo(std::string const & ps)		{_pseudo = ps; };
+		void		setPassword(std::string const & pw)		{_password = pw; };
+		void		setUsername(std::string const & us)		{_username = us; };
+		void		setFullname(std::string const & fu)		{_fullname = fu; };
 };
--- IRC/srcs/Client.cpp
+++ IRC/srcs/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:51:13 by mescobar          #+#    #+#             */
-/*   Updated: 2024/04/17 14:36:41 by qbanet           ###   ########.fr       */
+/*   Updated: 2024/04/17 15:43:41 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,41 @@
 
 //----------------------------------------------------------------------------||
 
+void	Client::linkSetMsg() const {
 
+	// * Msg d'arrivé à afficher sur le serveur
+}
+
+void	Client::joinChan(Channel *chan) {
+
+	// ? Ajoute le client par une fonction de Channel
+	_usrChan.push_back(chan);
+	// ? Oppération de vérif entre Client et Channel
+}
+
+void	Client::leaveChan(Channel * chan, bool kicked, std::string const& reason) {
+
+	if (kicked)
+		// * Supp du user dans le Channel(this, reason);
+
+	for (ChanVecIt it = _usrChan.begin(); it != _usrChan.end(); ++it) {
+        if (*it == chan) {
+            _usrChan.erase(it);
+            break;
+        }
+    }
+}
 
 //----------------------------------------------------------------------------||
 
-Client::Client(int fd, std::string const & pseudo) : _fd(fd), _pseudo(pseudo) {
+Client::Client(int fd, Server* serv, std::string const & pseudo)
+				: _fd(fd), _serv(serv), _pseudo(pseudo), _isConnected(false) {
 
 	std::clog << "Client " << pseudo << "crée avec le fd -> " << fd << std::endl;
 }
 
 Client::~Client(){
-	//something
+	// TODO : Ajouter la livération de mémoire si besoin
 }
 
 //----------------------------------------------------------------------------||
